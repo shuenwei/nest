ba0e3332611b81f075ac4d41c257a07d688e279b
shuenwei <gohshuenwei@gmail.com>
2025-05-29 20:46:31 +0800

    added pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from 'vite-plugin-pwa';
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
@@ -11,6 +12,25 @@ export default defineConfig({
     tsconfigPaths(),
 
     tailwindcss(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+       devOptions: {
+        enabled: true
+      },
+
+      pwaAssets: {
+      disabled: false,
+      config: true,
+      },
+
+      manifest: {
+        name: "nest",
+        short_name: "nest",
+        theme_color: "#ffffff",
+        display: "standalone"
+      }
+    })
   ],
 
   resolve: {
