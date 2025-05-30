9fcb5b25735417bb8fc6aa7b6f89a0f0fce829d4
shuenwei <gohshuenwei@gmail.com>
2025-05-30 11:30:48 +0800

    added padding

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,12 @@ const config: Config = {
       fontFamily: {
         outfit: ["'Outfit'", "sans-serif"],
       },
+      padding: {
+        "safe-top": "env(safe-area-inset-top)",
+        "safe-bottom": "env(safe-area-inset-bottom)",
+        "safe-left": "env(safe-area-inset-left)",
+        "safe-right": "env(safe-area-inset-right)",
+      },
     },
   },
   content: [
