7d5d20e7deb9c3d5b409ee81bca822c9e7904951
shuenwei <gohshuenwei@gmail.com>
2025-06-14 05:37:46 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,10 +13,13 @@ export default defineConfig({
 
     tailwindcss(),
 
-    /*VitePWA({
+    VitePWA({
       registerType: "autoUpdate",
-       devOptions: {
-        enabled: true
+      workbox: {
+        cleanupOutdatedCaches: true,
+      },
+      devOptions: {
+        enabled: true,
       },
 
       manifest: {
@@ -25,21 +28,21 @@ export default defineConfig({
         theme_color: "#ffffff",
         display: "standalone",
         icons: [
-      {
-        src: '/icon-192.png',
-        sizes: '192x192',
-        type: 'image/png',
-        purpose: 'any maskable'
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
       },
-      {
-        src: '/icon-512.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'any maskable'
-      }
-    ]
-      }
-    })*/
+    }),
   ],
 
   resolve: {
