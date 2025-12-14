5ea77f652d99ec5600faf8ad239756eecbbb20b9
shuenwei <gohshuenwei@gmail.com>
2025-12-14 15:40:00 +0200

    updated ui for telegram mini app

--- src/layouts/StandardLayout.tsx
+++ src/layouts/StandardLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const StandardLayout = () => {
     return (
-        <div className="pt-[env(safe-area-inset-top)] min-h-screen bg-[#F8F8F8]">
+        <div className="pt-[env(safe-area-inset-top)] [.telegram-webapp_&]:pt-[calc(env(safe-area-inset-top)+20px)] min-h-screen bg-[#F8F8F8]">
             <Outlet />
         </div>
     );
